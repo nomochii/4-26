4666b5ca7821bdf90a54a8f2348c50d8c373a4ef
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-06-15 12:06:02 +0000

    4-10終わり

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
-  get "/homes/about" => "homes#about", as: "about"
   get "/homes/top" => "homes#top", as: "top"
+  get "/homes/about" => "homes#about", as: "about"
+
   devise_for :users
 
   resources :post_images, only: [:new, :create, :index, :show]
